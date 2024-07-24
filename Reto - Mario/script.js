74aa627140c9b0c0e996850dc7ea8e9aefe31c6f
Mario Tudón <90724520+MarioTudon@users.noreply.github.com>
2024-07-23 18:02:15 -0600

    Add files via upload

---
+++ Reto - Mario/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('header nav').style.width = document.getElementById('header-title').offsetWidth + 'px';
+    document.querySelector('nav a');
+    document.getElementsByClassName('card');
+    document.querySelector('footer');
+});
